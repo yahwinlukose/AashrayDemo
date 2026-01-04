85b1052b5a503f5c0f7deac013e942dc04416f96
yahwinlukose <yahwinlukose@gmail.com>
2026-01-04 19:15:40 +0530

    second commit

--- aashray/settings.py
+++ aashray/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-aashray-dev-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
